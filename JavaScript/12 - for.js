d2134ec6ba4bc6ad4d3d3d60469b0e211936e6c5
caioagralemos <caioagralemos@live.com>
2023-02-06 21:02:54 -0300

    webdev

---
+++ JavaScript/12 - for.js
@@ -0,0 +1,36 @@
+// for (let i = 1; i < 10; i++) {
+//     console.log("Dona lurde")
+// }
+
+// for (let i = 0; i < 6; i++){
+//     console.log("Da ba dee da ba daa")
+// }
+
+// for (let i = 0; i <= 100; i+= 2){
+//     // numeros pares 0 a 100
+//     console.log(i)
+// }
+
+for (let i = 100; i >= 0; i-= 10){
+    console.log(i)
+}
+
+// looping over arrays
+
+meuarray = ["Arthur",
+            "Bruno",
+            "Caio",
+            "Davi",
+            "Everaldo",
+            "Fábio",
+            "Geraldo",
+            "Higor",
+            "James",
+            "Kaleb",
+            "Lucas",
+            "Mano",
+            "Nolan"]
+    
+for (let i = 0; i < meuarray.length; i++){
+    console.log(i, meuarray[i])
+}
