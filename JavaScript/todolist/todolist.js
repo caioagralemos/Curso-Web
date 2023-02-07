290f29cf73a47a99c9a6e961af9ab70454d3353d
caioagralemos <caioagralemos@live.com>
2023-02-07 00:49:57 -0300

    webdev

--- JavaScript/todolist/todolist.js
+++ JavaScript/todolist/todolist.js
@@ -1,24 +1,40 @@
-command = prompt("What would you like to do?")
-command = command.toLowerCase()
-list = []
+const listinha = []
 while(true){
-    if (command != "new" && command != "list" && command != "delete" && command != "quit"){
-        command = prompt("What would you like to do?")
+    let command = prompt("What would you like to do?")
+    command = command.toLowerCase()
+    if (command == "quit") {
+        alert("You've successfully quitted the program!")
+        break
+    }
+    else if (command != "new" && command != "list" && command != "delete"){
+        continue
     }
     else {
         if (command === "new"){
             newtodo = prompt("Name your new to-do: ")
-            list.push(newtodo)
-            console.log(`${newtodo} added to your list`)
+            listinha.push(newtodo)
+            alert(`${newtodo} added to your list`)
         }
         else if (command === "list"){
-            command.log(list)
+            let string = "**********\n"
+            for (let elemento in listinha){
+                string = string + `- ${listinha[elemento]}\n`
+            }
+            string = string + "**********"
+            alert(string)
+            continue
         }
         else if (command === "delete"){
             deltodo = prompt("What to-do would you like to delete?")
-        }
-        else if (command === "quit"){
-            break
+            let eai = listinha.indexOf(deltodo)
+            if (eai != -1){
+                listinha.splice(eai, 1)
+                alert(`${deltodo} removed from your list`)
+            }
+            else{
+                alert(`Error 404 not found\nThere is no ${deltodo} on your list`)
+            }
+            continue
         }
     }
 }
