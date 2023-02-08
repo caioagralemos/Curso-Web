a7fcefce8c8273211f725690e281bcc78f0e66f2
caioagralemos <caioagralemos@live.com>
2023-02-08 12:44:24 -0300

    webdev

--- JavaScript/21 - arraymethods.js
+++ JavaScript/21 - arraymethods.js
@@ -30,3 +30,61 @@ const areNumbersOdd = numbers.map((num) => (
 ))
 
 // - MÉTODO FILTER - cria um novo array com elementos que passam por um teste implementado pela função
+
+const movies = [
+    {
+        title: "Shrek",
+        score: 90,
+        year: 2002
+    },
+    {
+        title: "Toy Story",
+        score: 99,
+        year: 2001
+    },
+    {
+        title: "Sausage Party",
+        score: 30,
+        year: 2020
+    },
+    {
+        title: "Puss in Boots",
+        score: 64,
+        year: 2013
+    },
+    {
+        title: "Puss in Boots 2",
+        score: 90,
+        year: 2022
+    },
+    {
+        title: "Zootopia",
+        score: 74,
+        year: 2017
+    }
+]
+
+const goodMovies = movies.filter((m) => (
+    m.score > 75
+))
+
+const recentMovies = movies.filter(m => m.year >= 2020)
+
+movies
+    .filter(m => m.score > 80)
+    .map(m => m.title)
+    // para fins de pesquisa podemos fazer isso
+    
+// MÉTODO SOME - retorna um valor booleano se algum elemento do array passa em um teste implementado pela função
+
+numbers.some(num => (
+    num > 19 // true
+))
+
+// MÉTODO EVERY - retorna um valor booleano se todos os elementos do array passam em um teste implementado pela função
+
+numbers.every(num => (
+    num > 19 // false
+))
+
+// MÉTODO REDUCE - executa uma função em cada elemento do array até fazer o array ter um só elemento
